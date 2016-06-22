c972561fbcfffbe10babe7919cce900f87c68c2b
Jesse Zhang <jzhang@pivotal.io>
2016-06-22 00:42:17 -0700

    Well this is not Bash

--- ci/bump.sh
+++ ci/bump.sh
@@ -7,7 +7,7 @@ _main() {
 	cat version/number > develop/README.md
 	git -C develop add README.md
 	git -C develop add README.md
-	git -C develop -c user.name="ORCA Bot" -c user.email="sbjesse@gmail.com" commit -m "bump version to $(< version/number)"
+	git -C develop -c user.name="ORCA Bot" -c user.email="sbjesse@gmail.com" commit -m "bump version to $(cat version/number)"
 	git clone --bare develop output
 }
 
