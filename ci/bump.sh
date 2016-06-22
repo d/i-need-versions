05756be305bc16b5d65644d5a580eabab7f7b9bd
Jesse Zhang <jzhang@pivotal.io>
2016-06-22 00:35:07 -0700

    first stab at version bumping

---
+++ ci/bump.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -x
+set -e -u
+
+_main() {
+	cat version/number > develop/README.md
+	git -C develop add README.md
+	git -C develop add README.md
+	git -C develop -c user.name="ORCA Bot" -c user.email="sbjesse@gmail.com" commit -m "bump version to $(< version/number)"
+	git clone --bare develop output
+}
+
+_main "$@"
